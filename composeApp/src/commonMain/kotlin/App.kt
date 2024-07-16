5c5323ee8d5afadbcc69c61411f43aabd4bd8f3d
DJDrama <putzzang@naver.com>
2024-07-16 19:44:49 +0900

    Share Resources in Compose Multiplatform

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,21 +1,33 @@
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import kmpcomposepractice.composeapp.generated.resources.Res
+import kmpcomposepractice.composeapp.generated.resources.hello_world
+import kmpcomposepractice.composeapp.generated.resources.ic_android_black_24dp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        Box(
+        Column(
             modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(text = "KMP Practice")
+            // put image into composeResources >> drawable and "rebuild"
+            Image(painterResource(Res.drawable.ic_android_black_24dp), null)
+            Text(
+                text = stringResource(Res.string.hello_world)
+            )
         }
     }
 }
